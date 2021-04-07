feaf1fcca72dfc3a6aa5014650fa4a06d3eadead
diymen <81767659+diymen@users.noreply.github.com>
2021-04-07 09:33:54 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.1.2/g' openwrt/package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt /XXT build $(TZ=UTC-8 date "+%Y.&m.%d") @ Openwrt /g" openwrt/package/lean/default-settings/files/zzz-default-settings
